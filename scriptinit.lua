dfcbf03dd5255570e39834841537f9fad91b8527
speedsterkawaii <avtarvirdi82@gmail.com>
2024-07-20 20:55:29 -0400

    Update scriptinit.lua

--- scriptinit.lua
+++ scriptinit.lua
@@ -1,37 +1,55 @@
---shit too lazy to do in c
+-- NYX INIT SCRIPT hi skid how did you find this?
 
--- MACROS
+-- libraries, although these are just macros and redirect to C
 
-warn("Loaded")
-base64 = {} base64.encode = function(path) return base64_encode(path) end base64.decode = function(path) return base64_decode(path) end
-crypt = {} crypt.base64_encode = function(path) return base64_encode(path) end crypt.base64encode = function(path) return base64_encode(path) end  crypt.base64_decode = function(path) return base64_decode(path) end crypt.base64decode = function(path) return base64_decode(path) end
-crypt.base64 = {} crypt.base64.encode = function(path) return base64_encode(path) end  crypt.base64.decode = function(path) return base64_decode(path) end
+base64 = {}
+base64.encode = function(path) return base64_encode(path) end
+base64.decode = function(path) return base64_decode(path) end
+
+crypt = {} 
+crypt.base64_encode = function(path) return base64_encode(path) end 
+
+crypt.base64encode = function(path) return base64_encode(path) end 
+crypt.base64_decode = function(path) return base64_decode(path) end
+crypt.base64decode = function(path) return base64_decode(path) end
+
+crypt.base64 = {}
+crypt.base64.encode = function(path) return base64_encode(path) end 
+crypt.base64.decode = function(path) return base64_decode(path) end
 
 http = {} 
+http.request = function(url) assert(type(url) == "string", "invalid argument #1 to 'httpget' (string expected, got " .. type(url) .. ") ", 2)  local response = request({ Url = url; Method = "GET"; }).Body task.wait() return response end
+http_request = function(url) return http.request(url) end
 
-http.request = function(url) 
-assert(type(url) == "string", "invalid argument #1 to 'httpget' (string expected, got " .. type(url) .. ") ", 2) --thanks plusgiant
-local response = request({ Url = url; Method = "GET"; }).Body 
-task.wait()
-return response 
+rconsoleclear = function() end
+consoleclear = function() end
+
+rconsolecreate = function() end
+consolecreate = function() end
+
+rconsoledestroy = function() end
+consoledestroy = function() end
+
+rconsoleinput = function() end
+consoleinput = function() end
+
+rconsoleprint = function() end
+consoleprint = function() end
+
+rconsolesettitle = function() end
+rconsolename = function() end
+rconsolename = function() end
+
+-- attempted (at least) to recreate script functions
+
+checkcaller = function()
+    return true
 end
 
-function hookfunction(old, hook)--fake
-    return function(...)
-        local result = {hook(...)}
-        
-        local oldResult = {old(...)}
-
-        return unpack(result), unpack(oldResult)
-    end
+getcallingscript = function()
+    return "no calling script!"
 end
 
-error = function(msg)
-assert(typeof(msg) == "string", "argument #1 expects a string")
-msg = string.gsub(msg, "CoreGui.RobloxGui.Modules.Common.PolicyService:0:", "CoreGui.SignedModuleScript:")
-
-print(msg)
-end
 
 SAVED_METATABLE = SAVED_METATABLE or {}
 local lua_setmetatable = setmetatable
