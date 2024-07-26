4f90f3f11c8de1034eb47e565fc04881b7cc86df
speedsterkawaii <avtarvirdi82@gmail.com>
2024-07-25 23:41:29 -0400

    Create console.lua

---
+++ libs/console.lua
@@ -0,0 +1,36 @@
+-- what script actually uses these shit lol
+function rconsoleclear() 
+end
+
+function rconsolecreate() --todo: allocate console via bridge??? possible but i dont want to do this
+end
+
+function rconsoledestroy() 
+end
+
+function rconsoleinput() 
+end
+
+function rconsoleprint(txt) 
+end
+
+function consoleclear() 
+end
+
+function consolecreate() 
+end
+
+function consoledestroy() 
+end
+
+function consoleinput() 
+end
+
+function consoleprint(txt) 
+end
+
+function rconsolename(txt) 
+end
+
+function consolesettitle(txt) 
+end
