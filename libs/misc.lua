9dcce93edcf6ef494dbc26d6416841ea7e027f0b
speedsterkawaii <avtarvirdi82@gmail.com>
2024-07-28 00:40:11 -0400

    Update misc.lua

--- libs/misc.lua
+++ libs/misc.lua
@@ -49,8 +49,38 @@ end
 
 --[[
 why do people need so much aliases just stick to one
-this was for unc
 ]]
+-- i have 3 base64 decode and encode functions which 2 are in luau and one is in c
+-- and the c one has a problem where it doesnt decode it properly when sent the content
+--it has a url decoding problem so im just gonna stick to this temporarily
+local basedummy='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function base64_fake_encode(data)
+    return ((data:gsub('.', function(x) 
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and '1' or '0') end
+        return r
+    end) .. '0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c = 0
+        for i = 1, 6 do c = c + (x:sub(i, i) == '1' and 2 ^ (6 - i) or 0) end
+        return b:sub(c + 1, c + 1)
+    end) .. ({ '', '==', '=' })[#data % 3 + 1])
+end
+
+function base64_fake_decode(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r, f = '', (b:find(x) - 1)
+        for i = 6, 1, -1 do r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and '1' or '0') end
+        return r
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c = 0
+        for i = 1, 8 do c = c + (x:sub(i, i) == '1' and 2 ^ (8 - i) or 0) end
+        return string.char(c)
+    end))
+end
 
 crypt = {} -- store
 base64 = {}
@@ -102,7 +132,7 @@ crypt.generatebytes = function(size)
         randomBytes[i] = string.char(math.random(0, 255))
     end
     local byteString = table.concat(randomBytes)
-    return crypt.base64Encode(byteString)
+    return base64_fake_encode(byteString)
 end
 
 crypt.generatekey = function()
